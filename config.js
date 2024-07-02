968e54744ce52542bbb0ffc89165981fd4f449a9
Raj kumar saw <22mt0288@iitism.ac.in>
2024-07-03 00:51:48 +0530

    jnbhvg

---
+++ config.js
@@ -0,0 +1,7 @@
+export default {
+    JWT_SECRET : "<secret>",
+    EMAIL: "edwin.kerluke@ethereal.email", // testing email & password
+    PASSWORD : "aXR2aJUKvys2KxCeY7",
+    ATLAS_URI: "mongodb+srv://Sawraj5:Sawraj5%40@cluster0.bhqpcte.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+}
